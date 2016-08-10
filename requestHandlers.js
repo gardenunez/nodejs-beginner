42c64ce4b42e7cef5ae91ecb3d3dab186623bcf0
jorge jardines <gardenunez@gmail.com>
2016-08-11 01:32:06 +0200

    add request handles

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+    console.log("Reques handler 'start' was called");
+}
+
+function upload() {
+    console.log("Reques handler 'upload' was called");
+}
+
+exports.start = start;
+exports.upload = upload;
