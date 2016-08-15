a452ffb5e23997df9a7968b8904f9009de5a276b
jorge jardines <gardenunez@gmail.com>
2016-08-15 19:55:29 +0200

    bring server to response. Avoiding blocking..

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,26 @@
-function start() {
+var exec = require("child_process").exec;
+
+
+function start(response) {
     console.log("Reques handler 'start' was called");
+    var content = "empty";
+
+    exec("ls -lah", function(error, stdout, stderr)
+    {
+        response.writeHead(200, {"content-Type" : "text\plain"})
+        response.write(stdout);
+        response.end();
+    });
+
+    return content;
+    // return "Hello start";
 }
 
-function upload() {
+function upload(response) {
     console.log("Reques handler 'upload' was called");
+    response.writeHead(200, {"content-Type" : "text\plain"})
+    response.write("Hello upload");
+    response.end();
 }
 
 exports.start = start;
