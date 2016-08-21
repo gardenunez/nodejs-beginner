96e4074ae85b575ee1c4111279a756ceb4856fcc
jorge jardines <gardenunez@gmail.com>
2016-08-22 01:00:31 +0200

    add basic sql server stub

---
+++ createDb.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "root"
+});
+
+connection.query("CREATE DATABASE node", function(err) {
+    if(err){
+        console.log('Could not create databse "node"');
+    }
+});
+
+connection.query('use node', function(err){
+    if(err){
+        console.log('Could not switch to database "node"');
+    }
+});
+
+connection.query('create table test ' +
+                '(id INT(11) AUTO_INCREMENT,' +
+                'content VARCHAR(25), ' +
+                'PRIMARY KEY(id)) '
+, function(err){
+    if(err){
+        console.log('Could not create table "test"');
+    }
+});
+
+connection.query('INSERT INTO test (content) VALUES ("Hello")'); 
+connection.query('INSERT INTO test (content) VALUES ("World")'); 
+
+connection.end();
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 var server = require("./server");
-var router = require("./router")
+var router = require("./router");
+var mysql = require("mysql");
+
 var requestHandlers = require("./requestHandlers");
 
 var handle = {};
@@ -10,3 +12,34 @@ handle['/upload'] = requestHandlers.upload;
 handle['/file'] = requestHandlers.readFile;
 
 server.start(router.route, handle);
+
+//mysql
+var connection = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "node"
+    }
+);
+
+var query = connection.query('SELECT id, content from test');
+
+ query.on('error', function(err) { 
+      console.log('A database error occured:');
+       console.log(err); 
+     }); 
+
+ query.on('fields', function(fields) {
+      console.log('Received fields information.'); 
+     }); 
+
+ query.on('result', function(result) { 
+     console.log('Received result:'); 
+     console.log(result); 
+     }); 
+
+ query.on('end', function() { 
+      console.log('Query execution has finished.');
+       connection.end(); 
+    });
